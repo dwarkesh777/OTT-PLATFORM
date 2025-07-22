70d2ac1a83d9f381086034821bb62e11cf9fe30b
dwarkesh777 <mrdwarkesh65@gmail.com>
2025-07-22 19:26:37 +0530

    divid

---
+++ src/user.java
@@ -0,0 +1,92 @@
+class user {
+    long user_id;
+    String first_name;
+    String middle_name;
+    String last_name;
+    String email;
+    String password;
+    Long mobile_no;
+    int age;
+
+    public user(long user_id, String first_name, String middle_name, String last_name, String email, String password, Long mobile_no, int age) {
+        this.user_id = user_id;
+        this.first_name = first_name;
+        this.middle_name = middle_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.password = password;
+        this.mobile_no = mobile_no;
+        this.age = age;
+    }
+
+    public user(String userId, String d, String s, String s1, String mail) {
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getMiddle_name() {
+        return middle_name;
+    }
+
+    public void setMiddle_name(String middle_name) {
+        this.middle_name = middle_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getMobile_no() {
+        return mobile_no;
+    }
+
+    public void setMobile_no(Long mobile_no) {
+        this.mobile_no = mobile_no;
+    }
+
+    @Override
+    public String toString() {
+        return "user_id=" + user_id +
+                ", first_name='" + first_name + '\'' +
+                ", middle_name='" + middle_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", mobile_no=" + mobile_no
+                ;
+    }
+}
